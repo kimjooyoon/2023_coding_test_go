0f096c6259e59c7ee159f897d099c2283a723d97
kimjooyoon <asketeddy@gmail.com>
2023-01-24 14:57:06 +0900

    9

--- t009/main.go
+++ t009/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/134240
+func main() {
+	fmt.Printf("%v", solution([]int{1, 3, 4, 6}))
+}
+
+func solution(food []int) string {
+	var result string
+	for i, v := range food {
+		if i == 0 {
+			continue
+		}
+		for j := 0; j < v/2; j++ {
+			result = result + strconv.Itoa(i)
+		}
+	}
+	tmp := reverse(result)
+	result = result + "0"
+	result = result + tmp
+
+	return result
+}
+
+func reverse(s string) string {
+	rns := []rune(s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	return string(rns)
 }
