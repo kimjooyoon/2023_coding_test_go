d4190c28aed1e00a5fe8f311b0ccd8b61c1b16ee
kimjooyoon <asketeddy@gmail.com>
2023-01-26 15:23:32 +0900

    22, add InsertAfter test

--- t022_datastructure/linkedlist/singlelinkedlist/linkedlist_test.go
+++ t022_datastructure/linkedlist/singlelinkedlist/linkedlist_test.go
@@ -64,3 +64,24 @@ func TestLinkedList_PushFront(t *testing.T) {
 	assert.Equal(t, 1, l.GetAt(2).val)
 	assert.Nil(t, l.GetAt(3))
 }
+
+func TestLinkedList_InsertAfter(t *testing.T) {
+	var l LinkedList[int]
+
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	node := l.GetAt(1)
+	l.InsertAfter(node, 4)
+
+	assert.Equal(t, 4, l.Count2())
+	assert.Equal(t, 4, l.GetAt(2).val)
+	assert.Equal(t, 3, l.Back().val)
+
+	tempNode := &Node[int]{val: 10}
+	l.InsertAfter(tempNode, 200)
+
+	assert.Equal(t, 4, l.Count2())
+	assert.Equal(t, 4, l.GetAt(2).val)
+}
